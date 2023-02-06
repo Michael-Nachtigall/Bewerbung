14d480de529ee2dde25d0e01b8c94bd66e63368c
Michael-Nachtigall <124600486+Michael-Nachtigall@users.noreply.github.com>
2023-02-06 17:03:30 +0100

    Add files via upload

---
+++ JavaScript/app.js
@@ -0,0 +1,13 @@
+function togglepopup1() {
+    document.getElementById("popup-1").classList.toggle("active");
+
+}function togglepopup2() {
+    document.getElementById("popup-2").classList.toggle("active");
+
+}function togglepopup3() {
+    document.getElementById("popup-3").classList.toggle("active");
+
+}function togglepopup4() {
+    document.getElementById("popup-4").classList.toggle("active");
+
+}
